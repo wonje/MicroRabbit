45f5319b909130b5fb81db4ae38e8c477e5fb74c
Wonje Kang <wkang@softheon.com>
2020-07-24 00:33:44 -0400

    Implemented Publish and SendCommand for Banking MicroService

---
+++ MicroRabbit/MicroRabbit.Banking.Application/Models/AccountTransfer.cs
@@ -0,0 +1,11 @@
+﻿namespace MicroRabbit.Banking.Application.Models
+{
+    public class AccountTransfer
+    {
+        public int AccountFrom { get; set; }
+
+        public int AccountTo { get; set; }
+
+        public decimal TransferAmount { get; set; }
+    }
+}
