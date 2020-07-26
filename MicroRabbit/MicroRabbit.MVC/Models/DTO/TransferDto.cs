8dd38fbb94dece895ee0977287c7850bfaa55aff
wonje <wonjess92@gmail.com>
2020-07-25 23:44:06 -0400

    Add ASP.NET MVC Web Application for MicroService Testing

---
+++ MicroRabbit/MicroRabbit.MVC/Models/DTO/TransferDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MicroRabbit.MVC.Models.DTO
+{
+    public class TransferDto
+    {
+        public int FromAccount { get; set; }
+
+        public int ToAccount { get; set; }
+
+        public decimal TransferAmount { get; set; }
+    }
+}
---
+++ MicroRabbit/MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,26 @@
+﻿using MicroRabbit.MVC.Models.DTO;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _apiClient;
+
+        public TransferService(HttpClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task Transfer(TransferDto transferDto)
+        {
+            var uri = "https://localhost:5001/api/Banking";
+            var transferContent = new StringContent(JsonConvert.SerializeObject(transferDto), System.Text.Encoding.UTF8, "application/json");
+
+            var response = await _apiClient.PostAsync(uri, transferContent);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
